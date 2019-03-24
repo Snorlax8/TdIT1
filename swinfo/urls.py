f5171132462a852dd582be151aed9e1e462d3ef7
Cindy <ctarud@uc.cl>
2019-03-24 20:01:00 -0300

    Finished

--- swinfo/urls.py
+++ swinfo/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('characters/<int:character_id>/', views.character_detail, name='character_detail'),
     path('ships/<int:starship_id>/', views.ship_detail, name='ship_detail'),
     path('planets/<int:planet_id>/', views.planet_detail, name='planet_detail'),
+    path('search/', views.search),
 ]
--- swinfo/views.py
+++ swinfo/views.py
@@ -114,3 +114,77 @@ def planet_detail(request, planet_id):
         'films':films,
         'residents':residents,
     })
+
+
+def search(request):
+    if 'q' in request.GET:
+        q = request.GET['q']
+        movies_result = requests.get('https://swapi.co/api/films/'+'?search='+q)
+        movies = movies_result.json()
+        movies_sources = []
+        movies_sources.append(movies)
+        while movies["next"]:
+            movies = requests.get(movies["next"]).json()
+            movies_sources.append(movies)
+
+        character_result = requests.get('https://swapi.co/api/people/'+'?search='+q)
+        characters = character_result.json()
+        characters_sources = []
+        for personaje in characters["results"]:
+            head, partition, tail = personaje["url"].partition("people/")
+            character_id = tail[:-1]
+            personaje.update({"id":character_id})
+            characters_sources.append(personaje)
+        while characters["next"]:
+            characters = requests.get(characters["next"]).json()
+            for personaje in characters["results"]:
+                head, partition, tail = personaje["url"].partition("people/")
+                character_id = tail[:-1]
+                personaje.update({"id":character_id})
+                characters_sources.append(personaje)
+
+
+        planet_result = requests.get('https://swapi.co/api/planets/'+'?search='+q)
+        planets = planet_result.json()
+        planets_sources = []
+        for planeta in planets["results"]:
+            head, partition, tail = planeta["url"].partition("planets/")
+            planet_id = tail[:-1]
+            planeta.update({"id":planet_id})
+            planets_sources.append(planeta)
+        while planets["next"]:
+            planets = requests.get(planets["next"]).json()
+            for planeta in planets["results"]:
+                head, partition, tail = planeta["url"].partition("planets/")
+                planet_id = tail[:-1]
+                planeta.update({"id":planet_id})
+                planets_sources.append(planeta)
+
+
+        ship_result = requests.get('https://swapi.co/api/starships/'+'?search='+q)
+        ships = ship_result.json()
+        ships_sources = []
+        for nave in ships["results"]:
+            head, partition, tail = nave["url"].partition("starships/")
+            ship_id = tail[:-1]
+            nave.update({"id":ship_id})
+            ships_sources.append(nave)
+        while ships["next"]:
+            ships = requests.get(ships["next"]).json()
+            for nave in ships["results"]:
+                head, partition, tail = nave["url"].partition("starships/")
+                ship_id = tail[:-1]
+                nave.update({"id":ship_id})
+                ships_sources.append(nave)
+
+
+        return render(request, 'swinfo/search.html', {
+            'movies' : movies_sources,
+            'characters' : characters_sources,
+            'planets' : planets_sources,
+            'ships' : ships_sources,
+        })
+
+
+    else:
+        return HttpResponse("Por favor ingresa un término para buscar.")
